120a576a1ecd7c363c457165108974f3d138f443
YangTzuHung <j25889651556@gmail.com>
2021-09-19 16:33:16 +0800

    202109191634

--- push_notify.php
+++ push_notify.php
@@ -1,13 +1,13 @@
 <?php
 date_default_timezone_set("Asia/Taipei"); //設定時區為台北時區
 require_once('LINEBotXiaoFei.php'); //引入LINEBotXiaoFei.php發送code寫在LINEBotTiny
-//$channelAccessToken =getenv('CAT_TEST') ;//初始化 紀錄圖片的ID
-$channelAccessToken = getenv('CAT_XIAOFEI'); //初始化 小飛群的ID
+$channelAccessToken =getenv('CAT_TEST') ;//初始化 紀錄圖片的ID
+//$channelAccessToken = getenv('CAT_XIAOFEI'); //初始化 小飛群的ID
 $client = new LINENotifyXiaoFei($channelAccessToken); //把Token,Secret丟到LINENotifyXiaoFei建立連線
 function WorkSchedule($time, $client)
 {
 	include('./connect.php'); //連結資料庫設定
-	$timecount = (strtotime($time) - strtotime("2021-02-21 00:00:00")) / (60 * 60 * 24); //相隔天數
+	$timecount = (strtotime($time) - strtotime("2021-09-19 00:00:00")) / (60 * 60 * 24); //相隔天數
 	$weekcount = floor($timecount / 7); //相隔週數
 
 	//今天單周還雙周
@@ -27,32 +27,36 @@ function WorkSchedule($time, $client)
 	$row_name = mysqli_fetch_assoc(mysqli_query($db_connection, $sql));
 	$name = $row_name["name"];
 
+    //回傳變數初始化
+    $returntext = "";
+	
 	if ($name == "") {  //檢查是否是替補日
-		$tempor = 0; //初始化
-		$tempor = $tempor + $weekcount;  //替補計算
-		$tempor = $tempor % 12; //因為有12個人，所以每12次重新一次
-		//查詢替補
-		$sql = "select * from duty_turn where id = " . $tempor;
-		$row_trun = mysqli_fetch_assoc(mysqli_query($db_connection, $sql));
-		$turn = $row_trun["name"];
-		//傳輸訊息
-		$client->pushtonotify(
-			$message = array(
-				'message' => "\n=======================\n     " . $time . "(" . $week . ")" . $day . "(替補)\n=======================\n--->" . $turn
-			)
-		);
-	} else {   //不是替補日
-		//傳輸訊息
-		$client->pushtonotify(
-			$message = array(
-				'message' => "\n=======================\n     " . $time . "(" . $week . ")" . $day . "\n=======================\n--->" . $name
-			)
-		);
-	}
+		$tempor = 6; //初始化 上次替補結尾輪到6號
+        $tempor = $tempor + floor($weekcount/2)*3;  //替補計算  兩個星期會有三次替補
+        if ( $oddandeven == 0 && $weekdaytempor == 0){
+            $tempor = $tempor % 11; 
+        }elseif($oddandeven == 0 && $weekdaytempor == 1){
+            $tempor = $tempor % 11 + 1; 
+        }else{  
+            $tempor = $tempor % 11 + 2;
+        }
+        //查詢替補
+        $sql = "select * from duty_turn where id = " . $tempor;
+        $row_trun = mysqli_fetch_assoc(mysqli_query($db_connection, $sql));
+        $turn = $row_trun["name"];
+        $returntext = "=======================\n     " . $time . "(" . $week . ")" . $day . "(替補)\n=======================\n--->" . $turn; // 回復訊息
+    } else {   //不是替補日
+        $returntext = "=======================\n     " . $time . "(" . $week . ")" . $day . "\n=======================\n--->" . $name; // 回復訊息
+    }
+	$client->pushtonotify(
+		$message = array(
+			'message' => $returntext
+		)
+	);
 	mysqli_close($db_connection);
 }
 //heroku 00:00會執行此檔案，但由於heroku沒在使用會進入休眠狀態，所以正常需要一分鐘緩衝時間，最慢五分鐘過
-if (date('H:i') == "00:00" || date('H:i') == "00:01" || date('H:i') == "00:02" || date('H:i') == "00:03" || date('H:i') == "00:04" || date('H:i') == "00:05") {
+if (date('H:i') == "00:00" || date('H:i') == "00:01" || date('H:i') == "00:02" || date('H:i') == "16:33" || date('H:i') == "16:34" || date('H:i') == "16:35") {
 	//標頭
 	$client->pushtonotify(
 		$message = array(
