36e9f7a3cef0e64ae8264d57fb5593fb24936faa
YangTzuHung <j25889651556@gmail.com>
2021-09-25 17:35:12 +0800

    202109251735

--- index.php
+++ index.php
@@ -140,7 +140,7 @@ foreach ($client->parseEvents() as $event) {
             $message = $event['message'];
             break;
         case 'postback': //隱藏訊息
-            $ReturnMessage = "postback";
+            $ReturnMessage = $event['data'];
             ReplyText($ReturnMessage, $event, $client); //回傳訊息
             break;
         case 'follow': //加為好友觸發
