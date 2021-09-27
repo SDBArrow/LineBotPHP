01917e54ec3a0410a1697459a7cdd31f62a14cbd
YangTzuHung <j25889651556@gmail.com>
2021-09-28 00:27:38 +0800

    202109280027

--- ChengeDuty.php
+++ ChengeDuty.php
@@ -42,7 +42,7 @@ function WorkSchedule($time)
 }
 
 //時間判斷
-if (date('H:i') == "00:00" || date('H:i') == "00:01" || date('H:i') == "00:02" || date('H:i') == "00:03" || date('H:i') == "00:04" || date('H:i') == "00:05") {
+if (date('H:i') == "00:27" || date('H:i') == "00:28" || date('H:i') == "00:29" || date('H:i') == "00:03" || date('H:i') == "00:04" || date('H:i') == "00:05") {
     //查詢今天值日生
     $time = date('Y-m-d');  //抓時間
     $today_duty = WorkSchedule($time); //丟去副程式WorkSchedule
