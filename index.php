3be80a8c26da54c0b30ef888d2b33c901b6da3bf
YangTzuHung <j25889651556@gmail.com>
2021-09-30 19:02:43 +0800

    202109301902

--- index.php
+++ index.php
@@ -184,7 +184,7 @@ switch (true) {
         ReplyText($ReturnMessage, $event, $client); //回傳訊息
         break;
     case ($message['text'] == "指令查詢" || $message['text'] == "指令" || $message['text'] == "指令介紹"): //指令介紹
-        $ReturnMessage = "指令表\n1.今日遛狗\n2.註冊\n3.工作檢核\n4.值日生權限 @名字(當日值日生有交換需由原本值日生給權限)\n5.更新註冊名字(Line有改名的話)\n6.班表\n7.日期查詢範例：2022-01-01\n8.注意事項\n9.明日遛狗\n10.餵食規則\n11.座位表\n12.地板物品\n13.排班 代碼 @人名\n14.抽";
+        $ReturnMessage = "指令表\n1.今日遛狗\n2.註冊\n3.工作檢核\n4.值日生權限 @名字(當日值日生有交換需由原本值日生給權限)\n5.更新註冊名字(Line有改名的話)\n6.班表\n7.日期查詢範例：2022-01-01\n8.注意事項\n9.明日遛狗\n10.後天遛狗\n11.換班規則\n12.餵食規則\n13.座位表\n14.地板物品\n15.排班 代碼 @人名\n16.抽";
         ReplyText($ReturnMessage, $event, $client); //回傳訊息
         break;
     case ($message['text'] == "注意" || $message['text'] == "注意事項"): //注意事項
@@ -193,6 +193,10 @@ switch (true) {
         break;
     case ($message['text'] == "餵食規則" || $message['text'] == "餵食"): //餵食
         $ReturnMessage = "一餐:\n1/8罐頭+70克飼料\n1/8罐頭+200公克水";
+        ReplyText($ReturnMessage, $event, $client); //回傳訊息
+        break;
+    case $message['text'] == "換班規則": //換班規則
+        $ReturnMessage = "換班規則：雙方同意即可申請換班，若更換至替補。將會於最後一次原班表值班完後再做更換。";
         ReplyText($ReturnMessage, $event, $client); //回傳訊息
         break;
     case ($message['text'] == "遛狗" || $message['text'] == "今日遛狗" || $message['text'] == "今天遛狗"): //今天遛狗
