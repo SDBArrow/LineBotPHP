85152e3eaaf42697cd80591f24102dcd2ea59e91
YangTzuHung <j25889651556@gmail.com>
2021-10-01 15:12:31 +0800

    202110011512

--- test/test3.php
+++ test/test3.php
@@ -2,5 +2,5 @@
 
 require_once('function_conform.php'); //引入LINEBotXiaoFei.php發送code寫在LINEBotTiny
 $time = date('Y-m-d');  //抓時間
-$client->WorkSchedule($time);
-echo $client;
+$reponse = $client->WorkSchedule($time);
+echo $reponse;
