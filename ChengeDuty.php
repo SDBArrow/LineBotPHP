19538cf351f4de4c2011581166d67721d01ca9d0
YangTzuHung <j25889651556@gmail.com>
2021-10-01 23:38:25 +0800

    202110012338

--- ChengeDuty.php
+++ ChengeDuty.php
@@ -1,59 +1,8 @@
 <?php
 include('./connect.php'); //連結資料庫設定
 date_default_timezone_set("Asia/Taipei"); //設定時區為台北時區
-
-// 處理遛狗查詢 
-function WorkSchedule($time)
-{
-    include('./connect.php'); //連結資料庫設定
-    $timecount = (strtotime($time) - strtotime("2021-09-19 00:00:00")) / (60 * 60 * 24); //相隔天數
-    $weekcount = floor($timecount / 7); //相隔週數
-    $oddandeven = $weekcount % 2;   //今天單周還雙周
-    $weekdaytempor = date('w', strtotime($time));  //今天星期幾
-
-    //查詢值日生
-    $sql = "select * from duty_list where day = " . $weekdaytempor . " and week = " . $oddandeven;
-    $row_userid = mysqli_fetch_assoc(mysqli_query($db_connection, $sql));
-    if (strtotime($time) - strtotime("2021-10-03 00:00:00")>=0){ //判斷新班表還舊班表
-        $userid = $row_userid["new_userid"];
-    }else{
-        $userid = $row_userid["userid"];
-    }
-
-    $ReturnMessage = "";     //回傳變數初始化
-
-    if ($userid == NULL) {  //檢查是否是替補日
-        $tempor = 6; //初始化 2021-09-19上次替補結尾輪到6號
-        $tempor = $tempor + floor($weekcount/2)*3;  //替補計算  兩個星期會有三次替補
-        if (strtotime($time) - strtotime("2021-10-03 00:00:00")>=0){ //判斷新班表還舊班表
-            if ( $oddandeven == 0 ){
-                $tempor = $tempor % 11;                 //兩個星期的第一次
-            }elseif($oddandeven == 1 && $weekdaytempor == 0){
-                $tempor = ($tempor % 11 + 1) % 11;             //兩個星期的第二次
-            }elseif($oddandeven == 0 && $weekdaytempor == 2){      
-                $tempor = ($tempor % 11 + 2) % 11;             //兩個星期的第三次
-            }
-        }else{
-            if ( $oddandeven == 0 && $weekdaytempor == 0){
-                $tempor = $tempor % 11;                 //兩個星期的第一次
-            }elseif($oddandeven == 0 && $weekdaytempor == 1){
-                $tempor = ($tempor % 11 + 1) % 11;             //兩個星期的第二次
-            }else{      
-                $tempor = ($tempor % 11 + 2) % 11;             //兩個星期的第三次
-            }
-        }
-        //查詢替補
-        $sql = "select * from duty_turn where id = ".$tempor;
-        $table_duty_trun = mysqli_fetch_assoc(mysqli_query($db_connection, $sql));
-        $userid = $table_duty_trun["userid"];
-        $ReturnMessage = $userid; // 回復訊息
-    } else {   //不是替補日
-        $ReturnMessage = $userid; // 回復訊息
-    }
-    //傳輸訊息
-    mysqli_close($db_connection); //關閉資料庫連線
-    return($ReturnMessage);
-}
+require('./function_conform.php'); //引入LINEBotXiaoFei.php發送code寫在LINEBotTiny
+$work = new Linebot();
 
 switch(true){
     //每天00:00要執行的
@@ -81,19 +30,19 @@ switch(true){
     //一星期執行一次、每個星期天執行
     case (date('w') == 0):
         $time = date('Y-m-d');  //抓時間
-        $duty_0 = WorkSchedule($time);
+        $duty_0 = $work ->WorkScheduleOnlyUserid($time);
         $time = date('Y-m-d', strtotime("+1 day"));  //抓時間
-        $duty_1 = WorkSchedule($time);
+        $duty_1 = $work ->WorkScheduleOnlyUserid($time);
         $time = date('Y-m-d', strtotime("+2 day"));  //抓時間
-        $duty_2 = WorkSchedule($time);
+        $duty_2 = $work ->WorkScheduleOnlyUserid($time);
         $time = date('Y-m-d', strtotime("+3 day"));  //抓時間
-        $duty_3 = WorkSchedule($time);
+        $duty_3 = $work ->WorkScheduleOnlyUserid($time);
         $time = date('Y-m-d', strtotime("+4 day"));  //抓時間
-        $duty_4 = WorkSchedule($time);
+        $duty_4 = $work ->WorkScheduleOnlyUserid($time);
         $time = date('Y-m-d', strtotime("+5 day"));  //抓時間
-        $duty_5 = WorkSchedule($time);
+        $duty_5 = $work ->WorkScheduleOnlyUserid($time);
         $time = date('Y-m-d', strtotime("+6 day"));  //抓時間
-        $duty_6 = WorkSchedule($time);
+        $duty_6 = $work ->WorkScheduleOnlyUserid($time);
         
         $sql[0] = "update sign_table set userid = null, e419_refrigerator = '', e419_refrigerator = '', e419_ashcan = '', e419_corridor = '', e419_conditioner_light = '', e420_corridor = '', e420_equipment = '', e420_chair = '', e420_conditioner_light = '', e420_Shoebox = '', room_conditioner_light = ''";
         $sql[1] = "update sign_table set userid = ".$duty_0." where day_int = 0";
