c190b81197e42fc7f202ffdc1b452879131e5b55
YangTzuHung <j25889651556@gmail.com>
2021-10-02 19:06:19 +0800

    202110021906

--- record.php
+++ record.php
@@ -39,7 +39,7 @@ class MYPDF extends TCPDF {
 <table cellpadding="1">
     <tr>
         <td style="border-bottom: 1px solid black; width: 100px;">星期</td>
-        <td style="border-bottom: 1px solid black; width: 90px;">姓名</td>
+        <td style="border-bottom: 1px solid black; width: 85px;">姓名</td>
         <td style="border-bottom: 1px solid black; width: 90px;">E419冰箱</td>
         <td style="border-bottom: 1px solid black; width: 95px;">E419倒垃圾</td>
         <td style="border-bottom: 1px solid black; width: 100px;">E419走廊清潔</td>
