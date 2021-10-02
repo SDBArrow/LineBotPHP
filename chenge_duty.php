2414d52c99fcdacef2d20a33000ed386a8c8bbc8
YangTzuHung <j25889651556@gmail.com>
2021-10-03 01:55:29 +0800

    202110030155

--- chenge_duty.php
+++ chenge_duty.php
@@ -39,7 +39,7 @@ switch(true){
             echo $ReturnMessage;
         }
             //每天00:00要執行的
-    case (date('H:i') == "01:51" || date('H:i') == "01:52" || date('H:i') == "01:53" || date('H:i') == "00:03" || date('H:i') == "00:04" || date('H:i') == "00:05"):
+    case (date('H:i') == "01:55" || date('H:i') == "01:56" || date('H:i') == "01:57" || date('H:i') == "00:03" || date('H:i') == "00:04" || date('H:i') == "00:05"):
         //清除昨天的值日生權限
         $sql = "update member set duty_level = 0";
         if(mysqli_query($db_connection, $sql)){ //更新到資料庫
