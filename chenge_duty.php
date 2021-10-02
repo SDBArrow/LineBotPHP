b6f1b5fe385f9c05fdc945bb09a0c81bd765c26e
YangTzuHung <j25889651556@gmail.com>
2021-10-03 01:26:25 +0800

    202110030126

--- chenge_duty.php
+++ chenge_duty.php
@@ -5,29 +5,6 @@ require('./function_conform.php'); //引入LINEBotXiaoFei.php發送code寫在LIN
 $work = new Linebot();
 
 switch(true){
-    //每天00:00要執行的
-    case (date('H:i') == "00:00" || date('H:i') == "00:01" || date('H:i') == "00:02" || date('H:i') == "00:03" || date('H:i') == "00:04" || date('H:i') == "00:05"):
-        //清除昨天的值日生權限
-        $sql = "update member set duty_level = ''";
-        if(mysqli_query($db_connection, $sql)){ //更新到資料庫
-            $ReturnMessage = "權限移除成功";
-        } else{
-            $ReturnMessage = "權限移除失敗";
-        }
-        //查詢今天值日生
-        $time = date('w');  //抓時間
-        $sql = "select * from sign_table where day_int = ".$time; 
-        $row = mysqli_fetch_assoc(mysqli_query($db_connection, $sql));
-        $today_duty= $row["userid"];
-        //新增權限給今日值日生
-        $sql = "update member set duty_level = 1 where userid =".$today_duty;
-        if(mysqli_query($db_connection, $sql)){ //更新到資料庫
-            $ReturnMessage = "權限更新成功";
-        }else{
-            $ReturnMessage = "權限更新失敗";
-        }
-        echo $ReturnMessage;
-    //一星期執行一次、每個星期天執行
     case (date('w') == 0):
         $time = date('Y-m-d');  //抓時間
         $duty_0 = $work ->WorkScheduleOnlyUserid($time);
@@ -61,6 +38,29 @@ switch(true){
             }
             echo $ReturnMessage;
         }
+            //每天00:00要執行的
+    case (date('H:i') == "00:00" || date('H:i') == "00:01" || date('H:i') == "00:02" || date('H:i') == "00:03" || date('H:i') == "00:04" || date('H:i') == "00:05"):
+        //清除昨天的值日生權限
+        $sql = "update member set duty_level = ''";
+        if(mysqli_query($db_connection, $sql)){ //更新到資料庫
+            $ReturnMessage = "權限移除成功";
+        } else{
+            $ReturnMessage = "權限移除失敗";
+        }
+        //查詢今天值日生
+        $time = date('w');  //抓時間
+        $sql = "select * from sign_table where day_int = ".$time; 
+        $row = mysqli_fetch_assoc(mysqli_query($db_connection, $sql));
+        $today_duty= $row["userid"];
+        //新增權限給今日值日生
+        $sql = "update member set duty_level = 1 where userid =".$today_duty;
+        if(mysqli_query($db_connection, $sql)){ //更新到資料庫
+            $ReturnMessage = "權限更新成功";
+        }else{
+            $ReturnMessage = "權限更新失敗";
+        }
+        echo $ReturnMessage;
+    //一星期執行一次、每個星期天執行
     default:
         break;
 }
