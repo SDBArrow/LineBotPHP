4d23df7f84c7bd2db7a559415207ab05ac074aef
YangTzuHung <j25889651556@gmail.com>
2021-10-05 19:42:51 +0800

    202110051942

--- test.php
+++ test.php
@@ -5,12 +5,12 @@ include('./connect.php'); //連結資料庫設定
 $sql = "SHOW COLUMNS FROM sign_table"; 
 $result = mysqli_query($db_connection, $sql);
 $rowtotal = mysqli_num_rows($result);
-echo $rowtotal;
+echo $rowtotal."\n";
 
 
 while($table_sign_table = mysqli_fetch_assoc($result)){
 	$Field[$i] = $table_sign_table['Field'];
-	echo $Field[$i];
+	echo $Field[$i]."\n";
 }
 echo "end";
 /*
