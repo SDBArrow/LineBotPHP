ee6ca1d84fe058f699a79277ee101b34e3e4d4a7
YangTzuHung <j25889651556@gmail.com>
2021-10-05 17:58:06 +0800

    202110051758

--- test.php
+++ test.php
@@ -3,5 +3,5 @@ date_default_timezone_set("Asia/Taipei"); //設定時區為台北時區
 
 include('./connect.php'); //連結資料庫設定
 $sql = "SHOW COLUMNS FROM sign_table"; 
-$table_sign_table = mysqli_fetch_assoc(mysqli_query($db_connection, $sql));
-echo $table_sign_table['Field'][0];
+$table_sign_table = mysqli_fetch_array(mysqli_query($db_connection, $sql));
+echo $table_sign_table[0];
