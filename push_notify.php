682e4853c40e4be037c926c17069f515e91b243b
YangTzuHung <j25889651556@gmail.com>
2021-10-06 01:42:11 +0800

    202110060142

--- push_notify.php
+++ push_notify.php
@@ -1,8 +1,8 @@
 <?php
 date_default_timezone_set("Asia/Taipei"); //設定時區為台北時區
 require_once('LINEBotXiaoFei.php'); //引入LINEBotXiaoFei.php發送code寫在LINEBotTiny
-//$channelAccessToken =getenv('CAT_TEST') ;//初始化 紀錄圖片的ID
-$channelAccessToken = getenv('CAT_XIAOFEI'); //初始化 小飛群的ID
+$channelAccessToken =getenv('CAT_TEST') ;//初始化 紀錄圖片的ID
+//$channelAccessToken = getenv('CAT_XIAOFEI'); //初始化 小飛群的ID
 $client = new LINENotifyXiaoFei($channelAccessToken); //把Token,Secret丟到LINENotifyXiaoFei建立連線
 require('./function_conform.php'); //引入LINEBotXiaoFei.php發送code寫在LINEBotTiny
 $work = new Linebot();
@@ -29,19 +29,51 @@ switch(true){
 		$ReturnMessage = "\n=======================\n          本周工作檢核紀錄\n=======================\n--->https://reurl.cc/WXqqYk";
 		$work -> notifypushText($ReturnMessage, $client); //回傳訊息
 		break;
-	//檢測打卡有沒有缺漏
-    case (date('H:i') == "23:30" || date('H:i') == "23:31" || date('H:i') == "23:32" || date('H:i') == "23:33" || date('H:i') == "23:34" || date('H:i') == "23:35"):
-		//標頭
+	//檢測打卡有沒有缺漏，並補上缺漏
+    case (date('H:i') == "01:42" || date('H:i') == "01:43" || date('H:i') == "01:44" || date('H:i') == "23:33" || date('H:i') == "23:34" || date('H:i') == "23:35"):
+		
 		include('./connect.php'); //連結資料庫設定
 		$time = date('w');  //抓時間
-		$sql = "select * from sign_table,member where sign_table.userid = member.userid and day_int = ".$time; 
-        $table_sign_table = mysqli_fetch_assoc(mysqli_query($db_connection, $sql));
-		if ($table_sign_table["e419_refrigerator"] == "" || $table_sign_table["e419_ashcan"] == "" || $table_sign_table["e419_corridor"] == "" || $table_sign_table["e419_conditioner_light"] == "" || $table_sign_table["e420_corridor"] == "" || $table_sign_table["e420_equipment"] == "" || $table_sign_table["e420_chair"] == "" || $table_sign_table["e420_conditioner_light"] == "" || $table_sign_table["e420_Shoebox"] == "" || $table_sign_table["room_conditioner_light"] == ""){
+
+		//查詢sign_table 資料表欄位
+		$sql = "SHOW COLUMNS FROM sign_table"; 
+		$result = mysqli_query($db_connection, $sql);
+		$rowtotal = mysqli_num_rows($result);
+		echo $rowtotal."\n";
+
+		//提取sign_table欄位名稱
+		for ($i = 0; $i < $rowtotal; $i++){
+			$table_sign_table = mysqli_fetch_assoc($result);
+			$Field[$i] = $table_sign_table['Field'];
+			echo $Field[$i];
+		}
+
+		//檢查工作項目打卡是否有遺漏
+		$Miss = FALSE;
+		for ($i = 2; $i < $rowtotal; $i++){
+			if ($table_sign_table[$Field[$i]] == ""){
+				$MISS = True;
+				$sql = "update sign_table set ".[$Field[$i]]." = '缺漏' where day_int = ".$time;
+				if(mysqli_query($db_connection, $sql)){ //更新到資料庫
+					$ReturnMessage = "已更新工作審核表\n";
+				} else{
+					$ReturnMessage = "更新失敗\n";
+				}
+			}
+		}
+
+		//查詢值日生名字
+		$sql = "select * from sign_table,member where sign_table.userid = member.userid and day_int = ".$time;
+		$table_sign_table = mysqli_fetch_assoc(mysqli_query($db_connection, $sql));
+
+		//有缺漏發送提醒訊息
+		if ($Miss){
 			$ReturnMessage = "\n=====工作檢核自動提醒====="; //丟去副程式WorkSchedule
 			$work -> notifypushText($ReturnMessage, $client); //回傳訊息
 			$ReturnMessage = "\n".$table_sign_table["name"]."\n=======================\n--->工作檢核表尚未完成或有缺漏，請盡速補正"; //丟去副程式WorkSchedule
 			$work -> notifypushText($ReturnMessage, $client); //回傳訊息
 		}
+
 		mysqli_close($db_connection);
         break;
     default:
