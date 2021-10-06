8062bae83f176821c2860bbac2a82340e550c49f
YangTzuHung <j25889651556@gmail.com>
2021-10-07 02:55:30 +0800

    202110070255

--- test.php
+++ test.php
@@ -1,30 +1,27 @@
 <?php
-require('vendor/autoload.php');
+require 'vendor/autoload.php'; // If you're using Composer (recommended)
+// Comment out the above line if not using Composer
+// require("<PATH TO>/sendgrid-php.php");
+// If not using Composer, uncomment the above line and
+// download sendgrid-php.zip from the latest release here,
+// replacing <PATH TO> with the path to the sendgrid-php.php file,
+// which is included in the download:
+// https://github.com/sendgrid/sendgrid-php/releases
 
-$hostname = 'smtp.cloudmta.net';
-$username = '4d62f948c506dea7';
-$password = 'AR5uSM78LDL1SiwoUUCVSQCQ';
-
-$transport = (new Swift_SmtpTransport($hostname, 587, 'tls'))
-  ->setUsername($username)
-  ->setPassword($password);
-
-$mailer = new Swift_Mailer($transport);
-
-
-//Creating an sending the email
-$message = (new Swift_Message())
-  ->setSubject('Hello from PHP SwiftMailer')
-  ->setFrom(['3b280a1883502f197cc8@cloudmailin.net'])
-  ->setTo(['j25889651556@gmail.com' => 'dogmission']);
-
-$headers = ($message->getHeaders())
-  -> addTextHeader('X-CloudMTA-Class', 'standard');
-
-$message->setBody(
-  '<body>'.
-  '<h1>hello from php</h1>'.
-  '</body>'
+$email = new \SendGrid\Mail\Mail(); 
+$email->setFrom("test@example.com", "Example User");
+$email->setSubject("Sending with SendGrid is Fun");
+$email->addTo("test@example.com", "Example User");
+$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$email->addContent(
+    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
 );
-$message->addPart('hello from PHP', 'text/plain');
-$mailer->send($message);
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
+}
