6c9ad166703f22ade4ccd887196e8a7c76ba9b34
YangTzuHung <j25889651556@gmail.com>
2021-10-11 04:17:22 +0800

    202110110417

--- send_email.php
+++ send_email.php
@@ -8,43 +8,47 @@ require 'vendor/autoload.php'; // If you're using Composer (recommended)
 // which is included in the download:
 // https://github.com/sendgrid/sendgrid-php/releases
 
-$timestart = date('Y-m-d', strtotime("-7 day"));  //抓時間
-$timeend = date('Y-m-d', strtotime("-1 day"));  //抓時間
-$send_email = getenv('email');
-include('./connect.php'); //連結資料庫設定
-$sql = "select * from member where security = 2";
-$reutrn_data = mysqli_query($db_connection, $sql);
-
-while ($table_member = $reutrn_data->fetch_assoc()) {
-    //收件者 email 和 name
-    $user_name = $table_member["name"];
-    $user_email = $table_member["email"];
-    //設定eamil發送者、接收者、內容
-    $email = new \SendGrid\Mail\Mail(); 
-    $email->setFrom($send_email, "dogmission"); //寄件人資訊
-    $email->setSubject($timestart." ~ ".$timeend." 工作檢核");
-    $email->addTo($user_email, $user_name);
-    $email->addContent("text/plain", $timestart."~".$timeend." 工作檢核");
-    $email->addContent(
-        "text/html", "<strong>請看副檔</strong>"
-    );
-    //附件檔案
-    $file_encoded = base64_encode(file_get_contents("https://dogmission.herokuapp.com/record.pdf"));
-    $email->addAttachment(
-        $file_encoded,
-        "application/pdf",
-        "record.pdf",
-        "attachment"
-    );
-    //發送email
-    $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-    try {
-        $response = $sendgrid->send($email);
-        print $response->statusCode() . "\n";
-        print_r($response->headers());
-        print $response->body() . "\n";
-    } catch (Exception $e) {
-        echo 'Caught exception: '. $e->getMessage() ."\n";
-    }
+switch(true){
+    //發送近三日遛狗名單和簽到表
+    case (date('H:i') == "00:00" || date('H:i') == "00:01" || date('H:i') == "00:02" || date('H:i') == "00:03" || date('H:i') == "00:04" || date('H:i') == "00:05"):
+        $timestart = date('Y-m-d', strtotime("-7 day"));  //抓時間
+        $timeend = date('Y-m-d', strtotime("-1 day"));  //抓時間
+        $send_email = getenv('email');
+        include('./connect.php'); //連結資料庫設定
+        $sql = "select * from member where security = 2";
+        $reutrn_data = mysqli_query($db_connection, $sql);
+        
+        while ($table_member = $reutrn_data->fetch_assoc()) {
+            //收件者 email 和 name
+            $user_name = $table_member["name"];
+            $user_email = $table_member["email"];
+            //設定eamil發送者、接收者、內容
+            $email = new \SendGrid\Mail\Mail(); 
+            $email->setFrom($send_email, "dogmission"); //寄件人資訊
+            $email->setSubject($timestart." ~ ".$timeend." 工作檢核");
+            $email->addTo($user_email, $user_name);
+            $email->addContent("text/plain", $timestart."~".$timeend." 工作檢核");
+            $email->addContent(
+                "text/html", "<strong>請看副檔</strong>"
+            );
+            //附件檔案
+            $file_encoded = base64_encode(file_get_contents("https://dogmission.herokuapp.com/record.pdf"));
+            $email->addAttachment(
+                $file_encoded,
+                "application/pdf",
+                "record.pdf",
+                "attachment"
+            );
+            //發送email
+            $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+            try {
+                $response = $sendgrid->send($email);
+                print $response->statusCode() . "\n";
+                print_r($response->headers());
+                print $response->body() . "\n";
+            } catch (Exception $e) {
+                echo 'Caught exception: '. $e->getMessage() ."\n";
+            }
+        }
+        mysqli_close($db_connection); //關閉資料庫連線
 }
-mysqli_close($db_connection); //關閉資料庫連線
