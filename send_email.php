69cd11640e3aaa3fcb7cec85783fcaa8752beb89
YangTzuHung <j25889651556@gmail.com>
2021-10-11 03:03:47 +0800

    202110110303

--- send_email.php
+++ send_email.php
@@ -8,11 +8,21 @@ require 'vendor/autoload.php'; // If you're using Composer (recommended)
 // which is included in the download:
 // https://github.com/sendgrid/sendgrid-php/releases
 
+$timestart = date('Y-m-d', strtotime("-1 day"));  //抓時間
+$timestart = date('Y-m-d', strtotime("-8 day"));  //抓時間
+$myemail = getenv('email');
+
+include('./connect.php'); //連結資料庫設定
+$sql = "select * from member where security = 2";
+$table_member = mysqli_fetch_assoc(mysqli_query($db_connection, $sql));
+$name = $table_member["name"];
+$email = $table_member["email"];
+
 $email = new \SendGrid\Mail\Mail(); 
-$email->setFrom("j2588965156@gmail.com", "dogmission");
-$email->setSubject("20211003~20211009 工作檢核");
-$email->addTo("j25889651556@gmail.com", "yang");
-$email->addContent("text/plain", "20211003~20211009 工作檢核");
+$email->setFrom($myemail, "dogmission"); //寄件人資訊
+$email->setSubject($timestart."~".$timeend." 工作檢核");
+$email->addTo($email, $name);
+$email->addContent("text/plain", $timestart."~".$timeend." 工作檢核");
 $email->addContent(
     "text/html", "<strong>請看副檔</strong>"
 );
