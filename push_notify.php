4755c6ff1c8e73b718fbdb523f7b8e5fee4c389f
YangTzuHung <j25889651556@gmail.com>
2021-11-24 01:05:13 +0800

    202111240105

--- push_notify.php
+++ push_notify.php
@@ -1,8 +1,8 @@
 <?php
 date_default_timezone_set("Asia/Taipei"); //設定時區為台北時區
 require_once('LINEBotXiaoFei.php'); //引入LINEBotXiaoFei.php發送code寫在LINEBotTiny
-//$channelAccessToken =getenv('CAT_TEST') ;//初始化 紀錄圖片的ID
-$channelAccessToken = getenv('CAT_XIAOFEI'); //初始化 小飛群的ID
+$channelAccessToken =getenv('CAT_TEST') ;//初始化 紀錄圖片的ID
+//$channelAccessToken = getenv('CAT_XIAOFEI'); //初始化 小飛群的ID
 $client = new LINENotifyXiaoFei($channelAccessToken); //把Token,Secret丟到LINENotifyXiaoFei建立連線
 require('./function_conform.php'); //引入LINEBotXiaoFei.php發送code寫在LINEBotTiny
 $work = new Linebot();
@@ -10,7 +10,7 @@ $work = new Linebot();
 //heroku 00:00會執行此檔案，但由於heroku沒在使用會進入休眠狀態，所以正常需要一分鐘緩衝時間，最慢五分鐘過
 switch(true){
     //發送近三日遛狗名單和簽到表
-    case (date('H:i') == "00:00" || date('H:i') == "00:01" || date('H:i') == "00:02" || date('H:i') == "00:03" || date('H:i') == "00:04" || date('H:i') == "00:05"):
+    case (date('H:i') == "01:05" || date('H:i') == "01:06" || date('H:i') == "01:07" || date('H:i') == "00:03" || date('H:i') == "00:04" || date('H:i') == "00:05"):
   		//標頭
 		$ReturnMessage = "\n=====每日自動提醒====="; //丟去副程式WorkSchedule
 		$work -> notifypushText($ReturnMessage, $client); //回傳訊息
--- schedule_0000.php
+++ schedule_0000.php
@@ -1,5 +1,5 @@
 <?php
 include('push_notify.php');
-include('recode.php');
-include('send_email.php');
-include('chenge_duty.php');
+//include('recode.php');
+//include('send_email.php');
+//include('chenge_duty.php');
