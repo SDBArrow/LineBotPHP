fef64d15df255430ebd52d9e5d240bd3e59dc257
YangTzuHung <j25889651556@gmail.com>
2021-11-28 08:44:55 +0800

    202111280844

--- chenge_duty.php
+++ chenge_duty.php
@@ -5,35 +5,23 @@ require('./function_conform.php'); //引入LINEBotXiaoFei.php發送code寫在LIN
 $work = new Linebot();
 
 switch(true){
-    //一星期執行一次、每個星期天執行
-    case (date('w') == 0 && (date('H:i') == "00:00" || date('H:i') == "00:01" || date('H:i') == "00:02" || date('H:i') == "00:03" || date('H:i') == "00:04" || date('H:i') == "00:05")):
-        //寄送當周工作檢核至管理員的email
-
-        //清空工作檢核表，並匯入當周新值日生
-        $time = date('Y-m-d');  //抓時間
-        $duty_0 = $work ->WorkScheduleOnlyUserid($time);
-        $time = date('Y-m-d', strtotime("+1 day"));  //抓時間
-        $duty_1 = $work ->WorkScheduleOnlyUserid($time);
-        $time = date('Y-m-d', strtotime("+2 day"));  //抓時間
-        $duty_2 = $work ->WorkScheduleOnlyUserid($time);
-        $time = date('Y-m-d', strtotime("+3 day"));  //抓時間
-        $duty_3 = $work ->WorkScheduleOnlyUserid($time);
-        $time = date('Y-m-d', strtotime("+4 day"));  //抓時間
-        $duty_4 = $work ->WorkScheduleOnlyUserid($time);
-        $time = date('Y-m-d', strtotime("+5 day"));  //抓時間
-        $duty_5 = $work ->WorkScheduleOnlyUserid($time);
-        $time = date('Y-m-d', strtotime("+6 day"));  //抓時間
-        $duty_6 = $work ->WorkScheduleOnlyUserid($time);
+    //一星期執行一次、每個星期天執行簽到表清除及匯入
+    case (date('w') == 0 && (date('H:i') == "08:45" || date('H:i') == "08:46" || date('H:i') == "08:47" || date('H:i') == "08:48" || date('H:i') == "00:04" || date('H:i') == "00:05")):
+       
+        //查詢下星期每天的值日生userid
+        for ($var = 0; $var < 7; $var++){
+            $time = date('Y-m-d', strtotime("+".$var." day"));  //抓時間
+            $duty[$var] = $work ->WorkScheduleOnlyUserid($time);
+        }
         
+        //清空資料表
         $sql[0] = "update sign_table set userid = null, e419_refrigerator = '', e419_refrigerator = '', e419_ashcan = '', e419_corridor = '', e419_conditioner_light = '', e420_corridor = '', e420_equipment = '', e420_chair = '', e420_conditioner_light = '', e420_Shoebox = '', room_conditioner_light = ''";
-        $sql[1] = "update sign_table set userid = ".$duty_0." where day_int = 0";
-        $sql[2] = "update sign_table set userid = ".$duty_1." where day_int = 1";
-        $sql[3] = "update sign_table set userid = ".$duty_2." where day_int = 2";
-        $sql[4] = "update sign_table set userid = ".$duty_3." where day_int = 3";
-        $sql[5] = "update sign_table set userid = ".$duty_4." where day_int = 4";
-        $sql[6] = "update sign_table set userid = ".$duty_5." where day_int = 5";
-        $sql[7] = "update sign_table set userid = ".$duty_6." where day_int = 6";
+        //匯入值日生
+        for ($var = 1; $var <= count($duty); $var++){
+            $sql[$var] = "update sign_table set userid = ".$duty[$var-1]." where day_int = ".$var-1;
+        }
 
+        //資料庫更新
         for($var = 0; $var < count($sql); $var++){
             if(mysqli_query($db_connection, $sql[$var])){ //更新到資料庫
                 $ReturnMessage = "檢核表更新成功\n";
@@ -43,7 +31,7 @@ switch(true){
             echo $ReturnMessage;
         }
     //每天00:00要執行的
-    case (date('H:i') == "00:00" || date('H:i') == "00:01" || date('H:i') == "00:02" || date('H:i') == "00:03" || date('H:i') == "00:04" || date('H:i') == "00:05"):
+    case (date('H:i') == "08:45" || date('H:i') == "08:46" || date('H:i') == "08:47" || date('H:i') == "08:48" || date('H:i') == "00:04" || date('H:i') == "00:05"):
         //清除昨天的值日生權限
         $sql = "update member set duty_level = 0";
         if(mysqli_query($db_connection, $sql)){ //更新到資料庫
