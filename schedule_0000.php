d6e84d323e5a43b33d51c3962d726033321c9c43
YangTzuHung <j25889651556@gmail.com>
2021-11-28 08:06:43 +0800

    202111280806

--- schedule_0000.php
+++ schedule_0000.php
@@ -1,5 +1,5 @@
 <?php
-include('push_notify.php');
+include_once('push_notify.php');
 file_get_contents('https://dogmission.herokuapp.com/record.php'); //產生pdf
-include('send_email.php');
-include('chenge_duty.php');
+include_once('send_email.php');
+include_once('chenge_duty.php');
