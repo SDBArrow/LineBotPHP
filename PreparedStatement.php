b0c03d7aecc099823505015811b0c76569dd2cec
YangTzuHung <j25889651556@gmail.com>
2022-01-23 16:44:19 +0800

    202201231644

--- PreparedStatement.php
+++ PreparedStatement.php
@@ -1,15 +1,17 @@
 <?php
 include('./connect.php'); //連結資料庫設定
 
-$name = 'grandpa'; #自訂變數
-$sql = 'SELECT * FROM users WHERE username=?'; #SQL指令 ?代表參數
+$name = '楊子弘'; #自訂變數
+$sql = 'SELECT * FROM member WHERE name=?'; #SQL指令 ?代表參數
 
 $stmt = $db_connection -> stmt_init(); #使用前初始化
 $stmt -> prepare($sql); #將SQL進行編譯
 $stmt -> bind_param('s',$name); #帶入參數值 's'代表一個string 若參數有兩個string則為'ss'以此類推
 $stmt -> execute(); #執行
-
-
+$stmt->bind_result($district);
+$stmt->fetch();
+echo($district);
 #使用完釋放資源
 $stmt -> close();
 $db_connection -> close();
+
