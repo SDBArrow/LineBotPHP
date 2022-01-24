125d58481b228b8de05a7ef0df3f781776b371ed
YangTzuHung <j25889651556@gmail.com>
2022-01-24 23:31:51 +0800

    202201242331

--- PreparedStatement.php
+++ PreparedStatement.php
@@ -8,9 +8,9 @@ $stmt = $db_connection -> stmt_init(); #使用前初始化
 $stmt -> prepare($sql); #將SQL進行編譯
 $stmt -> bind_param('s',$name); #帶入參數值 's'代表一個string 若參數有兩個string則為'ss'以此類推
 $stmt -> execute(); #執行
-$stmt->bind_result($district);
+$stmt->bind_result($a0,$a1,$a2,$a3,$a4,$a5);
 $stmt->fetch();
-echo($district);
+echo($a0.$a1.$a2.$a3.$a4.$a5);
 #使用完釋放資源
 $stmt -> close();
 $db_connection -> close();
